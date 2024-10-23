5b073ce3a7c4af7f57324e979b361778ab85518b
AllanEllis229 <allanellis229@gmail.com>
2024-10-23 20:19:58 +0100

    FPS Set to 60

--- main.py
+++ main.py
@@ -6,15 +6,17 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         pygame.Surface.fill(screen, (0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        print(dt)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    
-    
 
 if __name__ == "__main__":
     main()
