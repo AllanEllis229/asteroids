99c42d20807996e84a4840ddbcfd2d50dad7f479
AllanEllis229 <allanellis229@gmail.com>
2024-10-25 08:52:51 +0100

    Player Turning added

--- main.py
+++ main.py
@@ -10,12 +10,17 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Asteroids")
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+
+
     while True:
+        dt = clock.tick(60) / 1000
+        player.update(dt)
         pygame.Surface.fill(screen, (0, 0, 0))
         player.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60) / 1000
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
